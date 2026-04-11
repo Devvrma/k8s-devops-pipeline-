33f736f7638148ccd46a0ac5f008209c045bd1e1
Dev Vrma <devvrma25@gmail.com>
2026-04-11 17:52:28 +0530

    Update app.py

--- user-service/app.py
+++ user-service/app.py
@@ -31,4 +31,4 @@ def upload_to_s3():
         return jsonify({"status": "Error", "error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5002)
